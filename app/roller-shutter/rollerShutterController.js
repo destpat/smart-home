8f843d11d488bba43ef4fa61c026db1d1687ab30
Ficadiere Yoan <yoan.ficadiere@gmail.com>
2018-06-27 09:56:14 +0200

    add : callback for mqtt request

--- app/roller-shutter/rollerShutterController.js
+++ app/roller-shutter/rollerShutterController.js
@@ -2,86 +2,92 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const mqtt = require('mqtt');
-
 const mqttConfig = require('../config').mqtt;
 const rollerTimeConfig = require('../config').rollerTime;
 
 const client =  mqtt.connect(mqttConfig.url, { username : mqttConfig.username, password: mqttConfig.password });
-
-let getTimeToRolling = (room) => {
-  return rollerTimeConfig[room];
-}
 
 let currentPosition = 0;
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+let getTimeToRolling = (room) => {
+  return rollerTimeConfig[room];
+}
+
+let millisecondsToSonoffTime = (milliseconds) => {
+  if (milliseconds < 11100) {
+    return Math.round(milliseconds / 100);
+  }
+  if (milliseconds > 11100) {
+    return `1${Math.round(milliseconds / 1000)}`;
+  }
+}
+
 router.post('/down', (req, res) => {
-  client.publish( `cmnd/${req.body.room}/pulseTime1`, getTimeToRolling(req.body.room));
-  client.publish( `cmnd/${req.body.room}/power1`, '1');
-  res.status(200).send({
-    message: 'Rolling shutter is close'
+  let downTime = millisecondsToSonoffTime(getTimeToRolling(req.body.room)).toString();
+  client.publish(`cmnd/${req.body.room}/pulseTime1`, downTime , () => {
+    client.publish(`cmnd/${req.body.room}/power1`, '1', () => {
+      res.status(200).send({
+        message: 'Rolling shutter is close',
+        downTime: downTime
+      });
+    });
   });
 });
 
 router.post('/up', (req, res) => {
-  client.publish(`cmnd/${req.body.room}/pulseTime2`, getTimeToRolling(req.body.room));
-  client.publish(`cmnd/${req.body.room}/power2`, '1');
-  res.status(200).send({
-    message: 'Rolling shutter is open'
+  let upTime = millisecondsToSonoffTime(getTimeToRolling(req.body.room)).toString();
+  client.publish(`cmnd/${req.body.room}/pulseTime2`, upTime , () => {
+    client.publish(`cmnd/${req.body.room}/power2`, '1', () => {
+      res.status(200).send({
+        message: 'Rolling shutter is open',
+        upTime: upTime
+      });
+    });
   });
 });
 
-/**
-* @description
-* Descend le volet roulant de
-* la cuisine avec le pourcentage donnée
-* en paramétre
-*/
 
 router.post('/:percent', (req, res) => {
-  console.log('inside');
+  /**
+  * @description
+  * Descend le volet roulant de
+  * la cuisine avec le pourcentage donnée
+  * en paramétre
+  */
   let percentInt = parseInt(req.params.percent);
-
   if (percentInt > 0 && percentInt <= 100) {
-    let millisecondsToSonoffTime = (milliseconds) => {
-      if (milliseconds < 11100) {
-        return Math.round(milliseconds / 100);
-      }
-      if (milliseconds > 11100) {
-        return `1${Math.round(milliseconds / 1000)}`;
-      }
-    }
-
     let percentToMilliseconds = (percent) => {
       return percent * getTimeToRolling(req.body.room) / 100;
     }
-
     if (currentPosition < percentInt) {
-      let downTime = millisecondsToSonoffTime(percentToMilliseconds(percentInt) - percentToMilliseconds(currentPosition));
-      console.log(req.body);
-        console.log(`${req.body.room}/pulseTime1`);
-        console.log(downTime.toString());
-        client.publish(`cmnd/${req.body.room}/pulseTime1`, downTime.toString());
-        client.publish(`cmnd/${req.body.room}/power1`, '1');
-        currentPosition = percentInt;
-        res.status(200).send({
-          message: 'Done',
-          downTime :  downTime,
-          currentPosition: currentPosition
-        })
+      let downTime = millisecondsToSonoffTime(percentToMilliseconds(percentInt) - percentToMilliseconds(currentPosition)).toString();
+      client.publish(`cmnd/${req.body.room}/pulseTime1`, downTime, () => {
+        client.publish(`cmnd/${req.body.room}/power1`, '1', () => {
+          currentPosition = percentInt;
+          console.log(downTime);
+          res.status(200).send({
+            message: 'Done',
+            downTime :  downTime,
+            currentPosition: currentPosition
+          })
+        });
+      });
     }
     if (currentPosition > percentInt) {
-      let upTime = millisecondsToSonoffTime(percentToMilliseconds(currentPosition) - percentToMilliseconds(percentInt))
-      client.publish(`cmnd/${req.body.room}/pulseTime2`, upTime.toString());
-      client.publish(`cmnd/${req.body.room}/power2`, '1');
-      currentPosition = percentInt;
-      res.status(200).send({
-        message: 'Done',
-        upTime :  upTime,
-        currentPosition: currentPosition
-      })
+      let upTime = millisecondsToSonoffTime(percentToMilliseconds(currentPosition) - percentToMilliseconds(percentInt)).toString()
+      client.publish(`cmnd/${req.body.room}/pulseTime2`, upTime , () => {
+        client.publish(`cmnd/${req.body.room}/power2`, '1', () => {
+          currentPosition = percentInt;
+          res.status(200).send({
+            message: 'Done',
+            upTime :  upTime,
+            currentPosition: currentPosition
+          })
+        });
+      });
     }
   } else {
     res.status(400).send({
